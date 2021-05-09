690871ff5361886f82bca308d91b70991ea5bcc8
Krachi Crunch <khoii87@gmail.com>
2021-05-09 12:15:09 +0200

    Add: ✨ Generating 10 images by API call from unsplash

--- src/Pages/InfinityScroll/Components/ImageLoader/ImageLoader.js
+++ src/Pages/InfinityScroll/Components/ImageLoader/ImageLoader.js
@@ -1,15 +1,20 @@
+import { useState } from 'react';
+
 import classes from './ImageLoader.module.scss';
 
-function ImageLoader() {
+function ImageLoader({ ...photo }) {
   const { 'image-container': imageContainer } = classes;
+
+  const {
+    links: { html: photoUrl },
+    urls: { regular: photoSrc },
+    alt_description: photoDescription,
+  } = photo;
+
   return (
     <div class={imageContainer} id='image-container'>
-      <a href='#' target='_blank'>
-        <img
-          src='https://images.unsplash.com/photo-1593643946890-b5b85ade6451?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1229&q=80'
-          alt=''
-          title=''
-        />
+      <a href={photoUrl} target='_blank' rel='noreferrer'>
+        <img src={photoSrc} alt={photoDescription} title={photoDescription} />
       </a>
     </div>
   );
--- src/Pages/InfinityScroll/InfinityScrollApp.js
+++ src/Pages/InfinityScroll/InfinityScrollApp.js
@@ -1,16 +1,40 @@
+import { useState, useEffect } from 'react';
+
 import SquareLoader from './Components/Animation/SquareLoader';
 import ImageLoader from './Components/ImageLoader/ImageLoader';
 
 import classes from './InfinityScrollApp.module.scss';
 
 function InfinityScrollApp() {
+  let photoContent = '';
+  const [photoData, setPhotoData] = useState([]);
+  const [initialCount, setinitialCount] = useState(10);
+  const [initialLoading, setInitialLoading] = useState(true);
   const { 'main-container': mainContainer } = classes;
+
+  useEffect(() => {
+    const apiKey = 'azDEgfUl7cGcx8ALUFh1hFLFbAvQ8IGwnR29V5lpfXM';
+    let unsplashApi = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${initialCount}`;
+
+    fetch(unsplashApi)
+      .then((res) => res.json())
+      .then((photos) => {
+        if (initialLoading) setPhotoData(photos);
+      })
+      .catch((err) => console.log(err));
+
+    return setInitialLoading(false);
+  }, []);
+
+  if (initialLoading === false) {
+    photoContent = photoData.map((photo) => <ImageLoader {...photo} />);
+  }
 
   return (
     <main className={mainContainer}>
       <h1>UNSPLASH API - INFINITY SCROLL</h1>
       <SquareLoader />
-      <ImageLoader />
+      {photoContent}
     </main>
   );
 }
