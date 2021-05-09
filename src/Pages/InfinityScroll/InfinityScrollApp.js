b725013b66dfb7f76be8b113a114b6fc977ec125
Krachi Crunch <khoii87@gmail.com>
2021-05-09 18:29:11 +0200

    Merge: 🔀 from branch 'main'
    Add: ✨ Infinity Photo Scroll effect

--- src/Pages/InfinityScroll/InfinityScrollApp.js
+++ src/Pages/InfinityScroll/InfinityScrollApp.js
@@ -6,8 +6,8 @@ import ImageLoader from './Components/ImageLoader/ImageLoader';
 import classes from './InfinityScrollApp.module.scss';
 
 function InfinityScrollApp() {
-  let photoContent = '';
   const [photoData, setPhotoData] = useState([]);
+  const [photoContent, setPhotoContent] = useState([]);
   const [initialCount, setinitialCount] = useState(10);
   const [initialLoading, setInitialLoading] = useState(true);
   const { 'main-container': mainContainer } = classes;
@@ -22,18 +22,41 @@ function InfinityScrollApp() {
         if (initialLoading) setPhotoData(photos);
       })
       .catch((err) => console.log(err));
-
     return setInitialLoading(false);
   }, []);
 
   if (initialLoading === false) {
-    photoContent = photoData.map((photo) => <ImageLoader {...photo} />);
+    photoData.map((photo) => {
+      return <ImageLoader key={photo.id} initialLoading={initialLoading} {...photo} />;
+    });
   }
+
+  function generatePhotoHandler() {
+    const photoItem = photoData.map((photo) => (
+      <ImageLoader key={photo.id} initialLoading={initialLoading} {...photo} />
+    ));
+
+    setPhotoContent((prevPhoto) => {
+      return [...prevPhoto, photoItem];
+    });
+  }
+
+  window.addEventListener('scroll', () => {
+    const windowHeight = window.innerHeight + window.scrollY;
+    const fullHeightImage = document.body.offsetHeight - 1000;
+
+    if (windowHeight >= fullHeightImage) {
+      generatePhotoHandler();
+    }
+  });
 
   return (
     <main className={mainContainer}>
       <h1>UNSPLASH API - INFINITY SCROLL</h1>
       <SquareLoader />
+      {photoData.map((photo) => {
+        return <ImageLoader key={photo.id} initialLoading={initialLoading} {...photo} />;
+      })}
       {photoContent}
     </main>
   );
