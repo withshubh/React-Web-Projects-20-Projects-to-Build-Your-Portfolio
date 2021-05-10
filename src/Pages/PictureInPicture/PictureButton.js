e77094180b647e72a86c6d6de26d61b784a09605
Krachi Crunch <khoii87@gmail.com>
2021-05-10 23:58:54 +0200

    Add: ✨ Picture-in-picture functionality is working now
    Just share the window which one you want, then click "START"

--- src/Pages/PictureInPicture/PictureButton.js
+++ src/Pages/PictureInPicture/PictureButton.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import { forwardRef } from 'react';
 import classes from './PictureButton.module.scss';
 
-function PictureButton() {
+function PictureButton({ displayVideoHandler }, ref) {
   const { 'picture-body__button-container': PictureBodyButtonContainer } = classes;
   return (
-    <div class={PictureBodyButtonContainer}>
-      <button id='picture-button'>START</button>
+    <div className={PictureBodyButtonContainer}>
+      <button ref={ref} onClick={displayVideoHandler}>
+        START
+      </button>
     </div>
   );
 }
 
-export default PictureButton;
+export default forwardRef(PictureButton);
